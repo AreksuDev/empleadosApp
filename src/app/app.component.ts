1bc39b18682da82002884aec25da44b1afa9fded
Alejandro <alejandroalvaa91@gmail.com>
2024-03-20 08:14:00 +0100

    App de empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Empleado } from './empleado.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'empleadosApp';
+  titulo = 'Listado de empleados';
+  empleados:Empleado[] = [
+    new Empleado("Juan", "Díaz", "Presidente", 7500),
+    new Empleado("Ana", "Martín", "Directora", 5500),
+    new Empleado("María", "Fernández", "Jefa sección", 3500),
+    new Empleado("Laura", "López", "Administrativo", 4500),
+  ]
 }
